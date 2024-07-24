170a40c9744901542af810c9711832f159df24dd
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-25 01:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.addEventListener('resize', function() {
     let width = window.innerWidth;
     let height = window.innerHeight;
     let sizeInfo = document.querySelector('#sizeInfo');
-    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
+    sizeInfo.innerHTML = '<h1>Width: ' + width  + ' Height: ' + height + '</h1>';
 });
 document.addEventListener('DOMContentLoaded', function() {
     // Your code here
 	 let width = window.innerWidth;
     let height = window.innerHeight;
     let sizeInfo = document.querySelector('#sizeInfo');
-    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
+    sizeInfo.innerHTML = '<h1>Width: ' + width +  ' Height: ' + height + '</h1>';
 });
