b001126808598ddbdbc25733d1611f39ba14f15d
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-25 01:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ window.addEventListener('resize', function() {
     let sizeInfo = document.querySelector('#sizeInfo');
     sizeInfo.innerHTML = '<h1>Width: ' + width  + ' Height: ' + height + '</h1>';
 });
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code here
-	 let width = window.innerWidth;
-    let height = window.innerHeight;
-    let sizeInfo = document.querySelector('#sizeInfo');
-    sizeInfo.innerHTML = '<h1>Width: ' + width +  ' Height: ' + height + '</h1>';
-});
