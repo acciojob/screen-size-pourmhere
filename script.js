981a83bd85f86fcd3d8e28a9fe7332165368c08f
Mustafa <pourmhere@gmail.com>
2024-08-01 01:18:11 +0530

    added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
+
+    // asd
     let width = window.innerWidth;
     let height = window.innerHeight;
     let sizeInfo = document.querySelector('#sizeInfo');
-    sizeInfo.innerHTML = '<h1>Width: ' + width  + ' Height: ' + height + '</h1>';
+    sizeInfo.innerHTML = '<h1>Width: ' + width + ' Height: ' + height + '</h1>';
 });
